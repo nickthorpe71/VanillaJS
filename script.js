d64331b92a4ad1c9b96a41e3c7906c3997a1d2a0
Nick Thorpe <nickthorpe71@gmail.com>
2021-07-10 15:55:15 -0500

    add mouse input logging

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Canvas setup
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0;
+let gameFrame = 0;
+ctx.font = '50px Georgia';
+
 // Mouse Interactivity
+let canvasPosition = canvas.getBoundingClientRect();
+
+const mouse =  {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    click: false
+}
+canvas.addEventListener('mousedown', (event) => {
+    mouse.x = event.x - canvasPosition.left;
+    mouse.y = event.y - canvasPosition.top;
+    console.log(mouse.x, mouse.y);
+})
+
 // Player
 // Bubbles
 // Animation Loop
