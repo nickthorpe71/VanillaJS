9f5470933ae927b368d3e84e91cb0ac811695a99
Nick Thorpe <nickthorpe71@gmail.com>
2021-07-10 16:11:27 -0500

    add player movement

--- script.js
+++ script.js
@@ -17,11 +17,53 @@ const mouse =  {
     click: false
 }
 canvas.addEventListener('mousedown', (event) => {
+    mouse.click = true;
     mouse.x = event.x - canvasPosition.left;
     mouse.y = event.y - canvasPosition.top;
-    console.log(mouse.x, mouse.y);
+})
+canvas.addEventListener('mouseup', (event) => {
+    mouse.click = false;
 })
 
 // Player
+const player = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    radius: 50,
+    angle: 0,
+    frameX: 0,
+    frameY: 0,
+    frame: 0,
+    spriteWidth: 498,
+    spriteHeight: 327,
+}
+
+function update(entity) {
+    const dx = entity.x - mouse.x;
+    const dy = entity.y - mouse.y;
+    if (mouse.x != entity.x)
+        entity.x -= dx/30;
+    if (mouse.y != entity.y)
+        entity.y -= dy/30;
+}
+
+function draw(entity) {
+    ctx.fillStyle = 'red';
+    ctx.beginPath();
+    ctx.arc(entity.x, entity.y, entity.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+}
+
+
 // Bubbles
+
 // Animation Loop
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    update(player);
+    draw(player);
+    requestAnimationFrame(animate);
+}
+
+animate();
